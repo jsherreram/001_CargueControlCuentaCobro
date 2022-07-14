986bde53c6bf57187322e51ba944446e73fe3c36
jsherreramGrupoASD <jherreram@grupoasd.com.co>
2022-07-14 11:49:51 -0500

    Se agregan comentarios de propiedad intelectual de Grupo ASD S.A.S a todas los archivos .java.

--- src/co/com/grupoasd/payroll/entity/ControlCuentaCobro.java
+++ src/co/com/grupoasd/payroll/entity/ControlCuentaCobro.java
@@ -1,5 +1,12 @@
 /**
+ * Archivo: ControlCuentaCobro.java
+ * Fecha: 17/06/2022
+ * Todos los derechos de propiedad intelectual e industrial sobre esta aplicacion son de propiedad exclusiva del GRUPO ASD
+ * S.A.S. Su uso, alteracion, reproduccion o modificacion sin el debido consentimiento por escrito de GRUPO ASD S.A.S. quedan
+ * totalmente prohibidos.
  *
+ * Este programa se encuentra protegido por las disposiciones de la Ley 23 de 1982 y demas normas concordantes sobre derechos
+ * de autor y propiedad intelectual. Su uso no autorizado dara lugar a las sanciones previstas en la Ley.
  */
 package co.com.grupoasd.payroll.entity;
 
